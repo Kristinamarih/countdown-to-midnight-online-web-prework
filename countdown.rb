c24f86f0ddd17006b5c8251112217e173c570f7e
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-21 00:16:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@ def countdown_with_sleep
   sleep(1.second)
 end
   
-def countdown(number)
-  while number > 0 
-   puts "#{number} SECOND(S)!"
-  number -= 1 
+def countdown(x)
+  while x > 0 
+   puts "#{x} SECOND(S)!"
+  x -= 1 
  
 end
 puts "HAPPY NEW YEAR!"
